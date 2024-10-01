f251a88cc01f2683c2741b85f02aaa1c316c3f73
Developing-Wizard <talha.m00001@gmail.com>
2024-10-01 15:59:16 +0500

    DashboardScreen.kt is step up using TvLazyColumn

--- app/src/main/java/com/example/tamashatv/screens/DashboardScreen.kt
+++ app/src/main/java/com/example/tamashatv/screens/DashboardScreen.kt
@@ -1,126 +1,88 @@
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.tv.foundation.lazy.list.TvLazyColumn
+import androidx.tv.foundation.lazy.list.TvLazyRow
+import androidx.tv.foundation.lazy.list.items
+import androidx.tv.foundation.lazy.list.rememberTvLazyListState
 import com.example.tamashatv.R
-@Preview( widthDp = 1080,
-    heightDp = 720,
-    showBackground = true,
-    backgroundColor = 0x000000)
+
+//@Preview(
+//    widthDp = 1080,
+//    heightDp = 720,
+//    showBackground = true,
+//    backgroundColor = 0x000000
+//)
 @Composable
 fun DashboardScreen() {
-    val scrollState = rememberScrollState()
-    ConstraintLayout(
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth()
-            .background(Color.Black).verticalScroll(scrollState)
+    val lazyColumnScrollState = rememberTvLazyListState()
+    TvLazyColumn(
+        contentPadding = PaddingValues(16.dp),
+        modifier = Modifier.background(Color.Black),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.spacedBy(15.dp), state = lazyColumnScrollState
     ) {
-        val (mainColumn, recyclerContinueWatching, recyclerLastWatch, recyclerPopularMovies, txtContinueWatching, txtLastWatch, txtPopularMovies) = createRefs()
-//        LazyColumn(modifier = Modifier.constrainAs(mainColumn){
-//            top.linkTo(parent.top)
-//            start.linkTo(parent.start)
-//            end.linkTo(parent.end)
-//            bottom.linkTo(parent.bottom)
-//        }) {
-        Text(
-            text = "Continue Watching",
-            color = Color.White,
-            fontSize = 26.sp,
-            modifier = Modifier.constrainAs(txtContinueWatching) {
-                top.linkTo(parent.top, 10.dp)
-                start.linkTo(parent.start, 25.dp)
-            })
-        LazyRow(
-            modifier = Modifier
-                .constrainAs(recyclerContinueWatching) {
-                    top.linkTo(txtContinueWatching.bottom, 15.dp)
-                    start.linkTo(txtContinueWatching.start)
-                    end.linkTo(parent.end)
+        item {
+            Text("Continue Watching", color = Color.White, fontSize = 25.sp)
+        }
+        item {
+            TvLazyRow {
+                items(getLastWatchListData()) { items ->
+                    RecyclerLayout(items)
                 }
-                .fillMaxWidth()
-                .background(Color.Transparent),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            items(getContinueWatchingListData()) { item ->
-                RecyclerLayout(item)
             }
         }
-        Text(
-            text = "Last Watch",
-            color = Color.White,
-            fontSize = 26.sp,
-            modifier = Modifier.constrainAs(txtLastWatch) {
-                top.linkTo(recyclerContinueWatching.bottom, 25.dp)
-                start.linkTo(recyclerContinueWatching.start)
-            })
-        LazyRow(
-            modifier = Modifier
-                .constrainAs(recyclerLastWatch) {
-                    top.linkTo(txtLastWatch.bottom, 15.dp)
-                    start.linkTo(txtLastWatch.start)
+        item {
+            Text("Popular Movies ", color = Color.White, fontSize = 25.sp)
+        }
+        item {
+            TvLazyRow {
+                items(getPopularMoviesListData()) { items ->
+                    RecyclerLayout(items)
                 }
-                .fillMaxWidth()
-                .background(Color.Transparent),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            items(getLastWatchListData()) { item ->
-                RecyclerLayout(item)
             }
         }
-        Text(
-            text = "Popular Movies",
-            color = Color.White,
-            fontSize = 26.sp,
-            modifier = Modifier.constrainAs(txtPopularMovies) {
-                top.linkTo(recyclerLastWatch.bottom, 25.dp)
-                start.linkTo(recyclerLastWatch.start)
-            })
-        LazyRow(
-            modifier = Modifier
-                .constrainAs(recyclerPopularMovies) {
-                    top.linkTo(txtPopularMovies.bottom, 15.dp)
-                    start.linkTo(txtPopularMovies.start)
+        item {
+            Text("Your List", color = Color.White, fontSize = 25.sp)
+        }
+        item {
+            TvLazyRow {
+                items(getLastWatchListData()) { items ->
+                    RecyclerLayout(items)
                 }
-                .fillMaxWidth()
-                .height(200.dp)
-                .background(Color.Transparent),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            items(getPopularMoviesListData()) { item ->
-                RecyclerLayout(item)
             }
         }
+        item {
+            Text("Last Watched", color = Color.White, fontSize = 25.sp)
+        }
+        item {
+            TvLazyRow {
+                items(getLastWatchListData()) { items ->
+                    RecyclerLayout(items)
+                }
+            }
+        }
+
 
     }
-
 }
 
 
@@ -129,34 +91,39 @@ data class VideosList(val name: String, val smallBannerImage: Int) {
 
 @Composable
 fun RecyclerLayout(items: VideosList) {
-    ConstraintLayout {
+    ConstraintLayout(
+        modifier = Modifier
+            .focusable()
+    ) {
         val (card, image, text) = createRefs()
         Card(
             modifier = Modifier
                 .clip(RoundedCornerShape(10.dp))
-                .background(Color.Black)
-                .size(150.dp)
+                .background(Color.Transparent)
                 .constrainAs(card) {
-                    top.linkTo(parent.top)
+                    top.linkTo(parent.top, 10.dp)
                     start.linkTo(parent.start)
-                    bottom.linkTo(parent.bottom)
-                    end.linkTo(parent.end)
-                },
-           // border = BorderStroke(2.dp, Color.White)
-        ) {
-            ConstraintLayout {
+                    bottom.linkTo(parent.bottom, 10.dp)
+                    end.linkTo(parent.end, 10.dp)
+                }
+                .focusable(),
 
+            // border = BorderStroke(2.dp, Color.White)
+        ) {
+            ConstraintLayout(
+                modifier = Modifier
+                    .background(Color.Black)
+                    .clip(RoundedCornerShape(10.dp))
+                    .fillMaxSize()
+            ) {
                 Image(
                     painter = painterResource(id = items.smallBannerImage),
                     contentDescription = "",
                     modifier = Modifier
-                        .background(
-                            Color.Black
-                        )
-                        .fillMaxSize()
+                        .height(180.dp)
+                        .width(220.dp)
                         .constrainAs(image) {
                             top.linkTo(parent.top)
-                            bottom.linkTo(parent.bottom)
                             start.linkTo(parent.start)
                             end.linkTo(parent.end)
                         },
@@ -171,7 +138,7 @@ fun RecyclerLayout(items: VideosList) {
                             top.linkTo(image.bottom, margin = 10.dp)
                             start.linkTo(image.start)
                             end.linkTo(image.end)
-                            bottom.linkTo(parent.bottom)
+                            bottom.linkTo(parent.bottom, 5.dp)
                         })
             }
 
